af589b3c8e2e3936c1dcd6b1c6e33efd7e1d1b7b
Maik <maik.macs@hotmail.com>
2019-07-29 18:34:58 -0500

    Fix Heroku Build

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ require('dotenv').config();
 
 app.set('port', process.env.PORT || 3001);
 
-var config = require('./config');
 var Twit = require('twit');
-var T = new Twit(config);
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
+});
 
 app.get('/api/tweets', function(req, res) {
   const param = req.query.q;
